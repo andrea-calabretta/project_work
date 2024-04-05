58abd68d33a79c05a20161b5d883f1c7841933dd
Andrea Calabretta <andreacalabretta0@gmail.com>
2024-04-05 14:50:41 +0200

    feat(MathController): add add function

--- project_work/src/main/java/org/example/project_work/MathController.java
+++ project_work/src/main/java/org/example/project_work/MathController.java
@@ -1,23 +1,15 @@
 package org.example.project_work;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping(path = "math")
 public class MathController {
-    //localhost:8080/hello
-    @RequestMapping (path = "/hello")
-    public @ResponseBody
-    String hello() {
-        return "Hello World ;-)";
-    }
 
-    //localhost:8080/hello/Andrea
-    @RequestMapping (path = "/hello/{name}")
-    public @ResponseBody
-    String hello(@PathVariable String name) {
-        return "Hello World " + name + "!!";
+    @PostMapping(path = "/add")
+    public @ResponseBody float add(@RequestParam float num1, @RequestParam float num2){
+        float result = num1 + num2;
+        return result;
     }
 }
